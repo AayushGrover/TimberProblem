e10baf5beb3938b88d3b4cccc643cf0a0f3a984e
gulu42 <adityagulati.blr@gmail.com>
2018-04-19 02:54:04 +0530

    Version1

---
+++ timber.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int m1,n1;
+    cin >> m1 >> n1;
+    const int m = m1;
+    const int n = n1;
+    int arr[m+1][n+1];
+
+    //shifting the indexing
+    for(int i = 0 ; i < m+1 ; i++)
+        arr[i][0] = 0;
+    for(int i = 0 ; i < n+1 ; i++)
+        arr[0][i] = 0;
+
+    //taking the input
+    for(int i = 1 ; i < m+1 ; i++)
+        for(int j = 1 ; j < n+1 ; j++)
+            cin >> arr[i][j];
+
+    //creating the dp array
+    int v1,v2,v3,v4;
+    for(int i = 1 ; i < m+1 ; i++)
+    {
+         for(int j = 1 ; j < n+1 ; j++)
+         {
+             v1 = arr[i-1][j];//value above
+             v2 = arr[i][j-1];//value to the left
+             v3 = arr[i-1][j-1];//value to the top left
+
+             arr[i][j] = v1 + v2 - v3 + arr[i][j];
+            // cout << "(" << v1 << "," << v2 << "," << v3 << ")" << " ";
+            // cout << arr[i][j] << " ";
+         }
+        //  cout << endl;
+    }
+
+    int c,x1,y1,x2,y2;
+    cin >> c;
+
+    for(int i = 0 ; i < c ; i++)
+    {
+        cin >> x1 >> y1 >> x2 >> y2;
+        v1 = arr[x2][y2];//bottom right
+        v2 = arr[x2][y1-1];//bottom left
+        v3 = arr[x1-1][y2];//top right
+        v4 = arr[x1-1][y1-1];//top left
+        //shifted since data stored in bottom right corner
+
+        cout << (v1-v2-v3+v4) << endl;
+    }
+
+}
